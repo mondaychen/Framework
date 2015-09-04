4c5adc34cd5809c75c79429d8b4fc1f3510bc944
mondaychen <monday.chen@gmail.com>
2015-09-04 16:20:34 -0400

    implement -nv

--- src/cs4620/mesh/MeshConverter.java
+++ src/cs4620/mesh/MeshConverter.java
@@ -45,7 +45,45 @@ public class MeshConverter {
 	public static MeshData convertToVertexNormals(ArrayList<Vector3> positions, ArrayList<Vector3i> tris) {
 		MeshData data = new MeshData();
 
-		// TODO#A1 SOLUTION START
+		// #A1 SOLUTION START
+		data.vertexCount = positions.size();
+		data.indexCount = tris.size() * 3;
+
+		// Create Storage Spaces
+		data.positions = NativeMem.createFloatBuffer(data.vertexCount * 3);
+		data.normals = NativeMem.createFloatBuffer(data.vertexCount * 3);
+		data.indices = NativeMem.createIntBuffer(data.indexCount);
+
+		for (Vector3 vector: positions) {
+			data.positions.put(vector.x);
+			data.positions.put(vector.y);
+			data.positions.put(vector.z);
+		}
+
+		ArrayList<Vector3> normals = new ArrayList<>();
+		for (Vector3 vector: positions) {
+			normals.add(new Vector3(0, 0, 0));
+		}
+		for (Vector3i tri: tris) {
+			Vector3 v1 = positions.get(tri.x);
+			Vector3 v2 = positions.get(tri.y);
+			Vector3 v3 = positions.get(tri.z);
+			Vector3 triNormal = v1.clone().sub(v2).cross(v1.clone().sub(v3));
+			normals.get(tri.x).add(triNormal);
+			normals.get(tri.y).add(triNormal);
+			normals.get(tri.z).add(triNormal);
+		}
+		for (Vector3 normal: normals) {
+			normal.normalize();
+			data.normals.put(normal.x);
+			data.normals.put(normal.y);
+			data.normals.put(normal.z);
+		}
+		for (Vector3i tri: tris) {
+			data.indices.put(tri.x);
+			data.indices.put(tri.y);
+			data.indices.put(tri.z);
+		}
 		
 		// #SOLUTION END
 		
