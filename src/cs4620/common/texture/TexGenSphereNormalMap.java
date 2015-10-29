a5e1d55ef3032ab0d879153c4cdf0a8ab61e5906
mondaychen <monday.chen@gmail.com>
2015-10-29 14:18:40 -0400

    new way of getColor

--- src/cs4620/common/texture/TexGenSphereNormalMap.java
+++ src/cs4620/common/texture/TexGenSphereNormalMap.java
@@ -2,7 +2,9 @@ package cs4620.common.texture;
 
 import egl.math.Color;
 import egl.math.Colord;
+import egl.math.Matrix3;
 import egl.math.Vector2i;
+import egl.math.Vector3;
 import egl.math.Vector3d;
 
 public class TexGenSphereNormalMap extends ACTextureGenerator {
@@ -34,21 +36,46 @@ public class TexGenSphereNormalMap extends ACTextureGenerator {
 		float cellSize = 1.0f/resolution;
 		float actualBumpRadius = bumpRadius * cellSize;
 		
-		int row = (int)Math.round(u/cellSize);
-		int col = (int)Math.round(v/cellSize);
-		
 		float localU = u % cellSize - 0.5f * cellSize; // (-cellSize/2, cellSize/2)
 		float localV = v % cellSize - 0.5f * cellSize;
 		
 		float finalX = 0.5f;
 		float finalY = 0.5f;
 		
-		if(Math.sqrt(Math.pow(localU, 2) + Math.pow(localV, 2)) < actualBumpRadius){
-			finalX -= Math.sin(localU * Math.PI);
-			finalY -= Math.sin(localV * Math.PI);
+		Colord colord = new Colord(finalX, finalY, 1);
+		
+		if(Math.sqrt(Math.pow(localU, 2) + Math.pow(localV, 2)) > actualBumpRadius){
+			outColor.set(colord);
+			return;
 		}
 		
-		Colord colord = new Colord(finalX, finalY, 1);
+		float centerU = u - localU;
+		float centerV = v - localV;
+		
+		Vector3 centerNormal = getNormal(centerU, centerV);
+		
+		Vector3 thisNormal = getNormal(u, v);
+		Vector3 tangent = new Vector3(thisNormal.x, 0f, -thisNormal.z).normalize();
+		Vector3 bitangent = thisNormal.clone().cross(tangent).normalize();
+		
+		Matrix3 mTBN = new Matrix3(tangent, bitangent, thisNormal);
+		
+		Vector3 finalNormal = centerNormal.clone();
+		mTBN.mul(finalNormal);
+		colord.set(finalNormal.x, finalNormal.y, finalNormal.z);
+		System.out.println(colord);
 		outColor.set(colord);
+	}
+	
+	private Vector3 getNormal(float u, float v) {
+		float radius = 1;
+		float theta = 2f * (float)Math.PI * u;
+		float phi = (float)Math.PI * v;
+
+		float _x = (float)(Math.cos(theta) * Math.sin(phi) * radius);
+		float _z = (float)(Math.sin(theta) * Math.sin(phi) * radius);
+		float _y = (float)(-Math.cos(phi) * radius);
+		
+		return new Vector3(_x, _y, _z).normalize();
 	}
 }
