5e89d9125f1cb6712f39971e0fe7d42948358152
mondaychen <monday.chen@gmail.com>
2015-11-07 17:59:34 -0500

    fixup

--- src/cs4620/splines/BSpline.java
+++ src/cs4620/splines/BSpline.java
@@ -23,9 +23,9 @@ public class BSpline extends SplineCurve{
 
 		//Just putting this in a Matrix4 because we don't have arbitrary sized matrices
 		Matrix4 bspPoints = new Matrix4(p0.x, p0.y, 0, 0,
-							p1.x, p1.y, 0, 0,
-							p2.x, p2.y, 0, 0,
-							p3.x, p3.y, 0, 0);
+										p1.x, p1.y, 0, 0,
+										p2.x, p2.y, 0, 0,
+										p3.x, p3.y, 0, 0);
 		
 		Matrix4 bezPoints = bspToBez.mulBefore(bspPoints);
 		
