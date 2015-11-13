8a02d4037b3f5a5140020ba9ff3824bc0ad4b05f
hellolittlej <ggjbetty@gmail.com>
2015-11-13 12:29:40 -0500

    modify the bspline part

--- src/cs4620/splines/BSpline.java
+++ src/cs4620/splines/BSpline.java
@@ -16,19 +16,27 @@ public class BSpline extends SplineCurve{
 			float eps) {
 		//TODO A5 (Extra Credit)
 		//SOLUTION
-		Matrix4 bspToBez = new Matrix4(1/6f,4/6f,1/6f,0/6f,
-									   0/6f,4/6f,2/6f,0/6f,
-									   0/6f,2/6f,4/6f,0/6f,
-									   0/6f,1/6f,4/6f,1/6f);
+        
+		Matrix4 bezToHer = new Matrix4(-1f, 3f, -3f, 1f, 
+				                        3f, -6, 3f, 0f, 
+				                        -3f, 3f, 0f, 0f, 
+				                        1f, 0f, 0f, 0f);
+		
+		Matrix4 bspToHer = new Matrix4(-1/6f, 3/6f, -3/6f, 1/6f, 
+				                        3/6f, -6/6f, 3/6f, 0/6f, 
+				                        -3/6f, 0/6f, 3/6f, 0/6f, 
+				                         1/6f, 4/6f, 1/6f, 0/6f);
+        
+		Matrix4 bspToBez = bezToHer.invert().mulBefore(bspToHer); 
 
-		//Just putting this in a Matrix4 because we don't have arbitrary sized matrices
+		//Making a position matrix;
 		Matrix4 bspPoints = new Matrix4(p0.x, p0.y, 0, 0,
 										p1.x, p1.y, 0, 0,
 										p2.x, p2.y, 0, 0,
 										p3.x, p3.y, 0, 0);
 		
 		Matrix4 bezPoints = bspToBez.mulBefore(bspPoints);
-		
+				
 		p0 = new Vector2(bezPoints.m[0], bezPoints.m[4]);
 		p1 = new Vector2(bezPoints.m[1], bezPoints.m[5]);
 		p2 = new Vector2(bezPoints.m[2], bezPoints.m[6]);
